3f63bb2f743e1f048c5442767dd238424b1ab595
charlesr1971 <cdesign@btinternet.com>
2023-09-15 20:55:08 +0100

    2 files modified

--- src/app/example21/example21.component.ts
+++ src/app/example21/example21.component.ts
@@ -12,31 +12,14 @@ import {
   of,
   Observable,
   timer,
-  Subject,
-  NEVER,
-  interval,
   EMPTY,
+  concat,
+  defer,
 } from 'rxjs';
-import {
-  switchMap,
-  scan,
-  mapTo,
-  startWith,
-  tap,
-  shareReplay,
-  distinctUntilChanged,
-  map,
-  withLatestFrom,
-  takeWhile,
-  takeUntil,
-  repeat,
-} from 'rxjs/operators';
+import { switchMap, mapTo, map, catchError } from 'rxjs/operators';
 import { DemoService } from '../services/demo.service';
 
-interface CounterStateModel {
-  count: number;
-  isTicking: boolean;
-}
+type StopwatchAction = 'START' | 'STOP' | 'RESET' | 'END';
 
 @Component({
   selector: 'app-example21',
@@ -45,113 +28,104 @@ interface CounterStateModel {
     <img class="question-image" src="./assets/images/questions/qA7.png" />
     <div class="item-header-2">Implementation</div>
     <div class="item-header-2-content">
+      <h3>
+        <span class="time-text" #hoursField>{{ hours | number:'2.0' }}</span>
+        <span class="time-separator-text">:</span>
+        <span class="time-text" #minutesField>{{ minutes | number:'2.0' }}</span>
+        <span class="time-separator-text">:</span>
+        <span class="time-text" #secondsField>{{ seconds | number:'2.0' }}</span>
+      </h3>
       <button #startBtn>Start</button>
-      <button #stopBtn>Stop Timer</button>
+      <button #stopBtn>Stop</button>
+      <button #resetBtn>Reset</button>
     </div>
     <div class="item-header-3">Answer</div>
     <div class="item-content item-text">True</div>
     <div class="item-header-4">Notes</div>
-    <div class="item-content item-text"></div>
+    <div class="item-content item-text">Please look in the console, to see if the timer gets destroyed</div>
   `,
   styles: [],
 })
 export class Example21Component implements OnInit, OnDestroy {
   @ViewChild('startBtn', { static: true }) startBtn!: ElementRef;
   @ViewChild('stopBtn', { static: true }) stopBtn!: ElementRef;
+  @ViewChild('resetBtn', { static: true }) resetBtn!: ElementRef;
 
-  timer$: any;
+  @ViewChild('hoursField', { static: true })
+  hoursField!: ElementRef;
+  @ViewChild('minutesField', { static: true })
+  minutesField!: ElementRef;
+  @ViewChild('secondsField', { static: true })
+  secondsField!: ElementRef;
 
-  startClick$: any;
-  stopClick$: any;
+  timer$: Observable<number>;
 
   startValue = 0;
 
-  initialCounterState: CounterStateModel = {
-    count: this.startValue,
-    isTicking: false,
-  };
+  hours: any = '00';
+  minutes: any = '00';
+  seconds: any = '00';
 
-  patchCounterState = new Subject<Partial<CounterStateModel>>();
-  counterCommands$: any;
-  counterState$: Observable<CounterStateModel>;
-  isTicking$: any;
-  commandFromTick$: any;
-  commandFromReset$: any;
-
-  constructor(private demoService: DemoService) {}
+  constructor(
+    private demoService: DemoService,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    /* this.timer$ = this.demoService.timer$.subscribe((time: number) => {
-      console.log('Example21Component: ngOnInit(): time', time);
-    }); */
+    const start$: Observable<StopwatchAction> = fromEvent(
+      this.startBtn.nativeElement,
+      'click'
+    ).pipe(mapTo('START'));
+    const stop$: Observable<StopwatchAction> = fromEvent(
+      this.stopBtn.nativeElement,
+      'click'
+    ).pipe(mapTo('STOP'));
+    const reset$: Observable<StopwatchAction> = fromEvent(
+      this.resetBtn.nativeElement,
+      'click'
+    ).pipe(mapTo('RESET'));
 
-    setTimeout(() => {
-      this.startClick$ = fromEvent(this.startBtn.nativeElement, 'click');
-      this.stopClick$ = fromEvent(this.stopBtn.nativeElement, 'click');
+    this.createTimer(merge(start$, stop$, reset$)).subscribe((seconds) => {
+      const secondsFieldVal = seconds % 60;
+      const minutesFieldVal = Math.floor(seconds / 60) % 60;
+      const hoursFieldVal = Math.floor(seconds / 3600);
+      this.hours = hoursFieldVal;
+      this.minutes = minutesFieldVal;
+      this.seconds = secondsFieldVal;
+    });
+  }
 
-      merge(
-        this.startClick$.pipe(mapTo(true)),
-        this.stopClick$.pipe(mapTo(false))
-      )
-        .pipe(
-          switchMap((val) => (val ? interval(10) : EMPTY)),
-          mapTo(1),
-          scan((acc: number, curr: number) => acc + curr, this.startValue),
-          takeWhile((val) => val >= 0),
-          startWith(this.startValue),
-          takeUntil(this.stopClick$),
-          repeat()
-        )
-        .subscribe((val) => {
-          // counterDisplayHeader.innerHTML = val.toString();
-          console.log('Example21Component: ngOnInit(): time', val.toString());
-        });
-
-      /* this.counterCommands$ = merge(
-        this.startClick$.pipe(mapTo({ isTicking: true })),
-        this.stopClick$.pipe(mapTo({ ...this.initialCounterState })),
-        this.patchCounterState.asObservable()
-      );
-
-      this.counterState$ = this.counterCommands$.pipe(
-        startWith(this.initialCounterState),
-        scan(
-          (counterState: CounterStateModel, command): CounterStateModel => ({
-            ...counterState,
-            ...command,
-          })
-        ),
-        shareReplay(1)
-      );
-
-      this.isTicking$ = this.counterState$.pipe(
-        map((state) => state.isTicking),
-        distinctUntilChanged()
-      );
-
-      this.commandFromTick$ = this.isTicking$.pipe(
-        switchMap((isTicking) => (isTicking ? timer(0, 10) : NEVER)),
-        withLatestFrom(this.counterState$, (_, counterState) => ({
-          count: counterState.count,
-        })),
-        tap(({ count }) => {
-          if (count) {
-            this.patchCounterState.next({ count: count - 1 });
-          } else {
-            this.patchCounterState.next({ ...this.initialCounterState });
+  createTimer(
+    control$: Observable<StopwatchAction>,
+    interval = 1000
+  ): Observable<number> {
+    return defer(() => {
+      let toggle: boolean = false;
+      let count: number = 0;
+      this.timer$ = timer(0, interval).pipe(map((x) => count++));
+      const end$ = of('END');
+      return concat(control$, end$).pipe(
+        catchError((_) => end$),
+        switchMap((control) => {
+          if (control === 'START' && !toggle) {
+            toggle = true;
+            return this.timer$;
+          } else if (control === 'STOP' && toggle) {
+            toggle = false;
+            return EMPTY;
+          } else if (control === 'RESET') {
+            count = 0;
+            this.hours = 0;
+            this.minutes = 0;
+            this.seconds = 0;
+            if (toggle) {
+              return this.timer$;
+            }
           }
+          return EMPTY;
         })
       );
-
-      this.commandFromReset$ = this.stopClick$.pipe(
-        mapTo({ ...this.initialCounterState })
-      );
-
-      merge(this.commandFromTick$, this.commandFromReset$)
-        .pipe(startWith(this.initialCounterState))
-        .subscribe((state: any) => {
-          console.log('Example21Component: ngOnInit(): time', state.count.toString());
-        }); */
     });
   }
 
