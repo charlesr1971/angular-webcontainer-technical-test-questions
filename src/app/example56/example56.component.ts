755e7c72a50fa9a6459d287cb4cc2f3b6b1a1f80
Charles Robertson <cdesign@btinternet.com>
2023-09-20 15:36:45 +0000

    Update 19 files

---
+++ src/app/example56/example56.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-example56",
+  template: `
+    <div class="item-header-1">Question</div>
+    <img class="question-image" src="./assets/images/questions/qA41.png" />
+    <div class="item-header-3">Answer</div>
+    <div class="item-content item-text">ElementRef</div>
+  `,
+  styles: [],
+})
+export class Example56Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
