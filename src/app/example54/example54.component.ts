5bbf328e0253ce1c209876260e2c36121cbe0bfb
Charles Robertson <cdesign@btinternet.com>
2023-09-20 15:36:45 +0000

    Update 19 files

---
+++ src/app/example54/example54.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-example54",
+  template: `
+    <div class="item-header-1">Question</div>
+    <img class="question-image" src="./assets/images/questions/qA39.png" />
+    <div class="item-header-3">Answer</div>
+    <div class="item-content item-text">()</div>
+  `,
+  styles: [],
+})
+export class Example54Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/example55/example55a.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Example55bComponent } from "./example55b.component";
+
+@Component({
+  selector: "app-example55a",
+  template: `
+    <div class="item-header-1">Question</div>
+    <img class="question-image" src="./assets/images/questions/qA40.png" />
+    <div class="item-header-2">Implementation</div>
+    <div class="item-header-2-content">
+      <app-example55b name="Demo"></app-example55b>
+    </div>
+    <div class="item-header-3">Answer</div>
+    <div class="item-content item-text">Demo2</div>
+    <div class="item-header-4">Notes</div>
+    <div class="item-content item-text">
+      In order to get <em>@ViewChild</em> property initiated, you need to call
+      it, inside the <em>ngAfterViewInit</em> lifecycle hook:
+      <a href="https://stackoverflow.com/a/56958379/1606349" target="_blank"
+        >More information</a
+      >
+    </div>
+    <div class="item-header-5">Conflict</div>
+    <div class="item-content item-text">The correct answer is not shown</div>
+  `,
+  styles: [],
+})
+export class Example55aComponent implements OnInit {
+  @ViewChild(Example55bComponent) child: Example55bComponent;
+
+  ngOnInit(): void {
+    console.log(
+      "Example55aComponent: ngOnInit(): this.child.name: ",
+      this.child.name,
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.child.name = "Demo2";
+    console.log(
+      "Example55aComponent: ngAfterViewInit(): this.child.name: ",
+      this.child.name,
+    );
+  }
+}
